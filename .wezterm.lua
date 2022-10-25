cf6b8cc50a4d938ed27e48b443e437f084980c5e
Orlando Romo <orlando.romo.97@hotmail.com>
2022-10-25 12:54:17 -0500

    New nvim configuration :)

--- .wezterm.lua
+++ .wezterm.lua
@@ -1,15 +1,1 @@
-local wezterm = require 'wezterm'
-return {
-  font = wezterm.font('FiraCode Nerd Font'),
-  font_size = 15,
-  -- color_scheme = "PencilDark", 
-  color_scheme = "DoomOne", 
-
-  keys = {
-    {
-      key = 'e',
-      mods = 'CTRL',
-      action = wezterm.action.SplitHorizontal { domain = 'CurrentPaneDomain'}
-    }
-  }
-}
+/Users/orlando.romo/.wezterm.lua
