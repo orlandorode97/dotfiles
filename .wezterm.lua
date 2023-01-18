b269a5c7c8aff50a05e0c219c08dd343feb0dc34
Orlando Romo <orlando.romo.97@hotmail.com>
2023-01-18 14:53:53 -0600

    wezterm update

--- .wezterm.lua
+++ .wezterm.lua
@@ -1,10 +1,386 @@
+--local wezterm = require("wezterm")
+--return {
+--	font = wezterm.font("Hack Nerd Font Mono"),
+--  bold = true,
+--	font_size = 16,
+--	color_scheme = "Catppuccin Frappe",
+--	check_for_updates = false,
+--	keys = {
+--		{
+--			key = "e",
+--			mods = "CMD",
+--			action = wezterm.action.SplitHorizontal({ domain = "CurrentPaneDomain" }),
+--		},
+--		{
+--			key = "d",
+--			mods = "CMD",
+--			action = wezterm.action.SplitVertical({ domain = "CurrentPaneDomain" }),
+--		},
+--		{
+--			key = "w",
+--			mods = "CMD",
+--			action = wezterm.action.CloseCurrentPane({ confirm = false }),
+--		},
+--	},
+--	enable_tab_bar = true,
+--	tab_bar_at_bottom = true,
+--	use_fancy_tab_bar = false,
+--	colors = {
+--		tab_bar = {
+--			background = "#232634",
+--			-- The active tab is the one that has focus in the window
+--			active_tab = {
+--				-- The color of the background area for the tab
+--				bg_color = "#ca9ee6",
+--				-- The color of the text for the tab
+--				fg_color = "#282a39",
+--
+--				-- Specify whether you want "Half", "Normal" or "Bold" intensity for the
+--				-- label shown for this tab.
+--				-- The default is "Normal"
+--				intensity = "Bold",
+--
+--				-- Specify whether you want "None", "Single" or "Double" underline for
+--				-- label shown for this tab.
+--				-- The default is "None"
+--				underline = "None",
+--
+--				-- Specify whether you want the text to be italic (true) or not (false)
+--				-- for this tab.  The default is false.
+--				italic = false,
+--
+--				-- Specify whether you want the text to be rendered with strikethrough (true)
+--				-- or not for this tab.  The default is false.
+--				strikethrough = false,
+--			},
+--			new_tab = {
+--				bg_color = "#232634",
+--				fg_color = "#c4ccec",
+--				-- The same options that were listed under the `active_tab` section above
+--				-- can also be used for `new_tab`.
+--			},
+--
+--			-- Inactive tabs are the tabs that do not have focus
+--			inactive_tab = {
+--				bg_color = "#282c3c",
+--				fg_color = "#c4ccec",
+--
+--				-- The same options that were listed under the `active_tab` section above
+--				-- can also be used for `inactive_tab`.
+--			},
+--		},
+--	},
+--}
 local wezterm = require("wezterm")
+local is_dark = true
+
+local function is_vi_process(pane)
+	return pane:get_foreground_process_name():find("n?vim") ~= nil
+end
+
+local function conditional_activate_pane(window, pane, pane_direction, vim_direction)
+	if is_vi_process(pane) then
+		window:perform_action(wezterm.action.SendKey({ key = vim_direction, mods = "ALT" }), pane)
+	else
+		window:perform_action(wezterm.action.ActivatePaneDirection(pane_direction), pane)
+	end
+end
+
+wezterm.on("ActivatePaneDirection-right", function(window, pane)
+	conditional_activate_pane(window, pane, "Right", "l")
+end)
+wezterm.on("ActivatePaneDirection-left", function(window, pane)
+	conditional_activate_pane(window, pane, "Left", "h")
+end)
+wezterm.on("ActivatePaneDirection-up", function(window, pane)
+	conditional_activate_pane(window, pane, "Up", "k")
+end)
+wezterm.on("ActivatePaneDirection-down", function(window, pane)
+	conditional_activate_pane(window, pane, "Down", "j")
+end)
+
+local catppuccin = {
+	dark = {
+		rosewater = "#F5E0DC",
+		flamingo = "#F2CDCD",
+		pink = "#F5C2E7",
+		mauve = "#CBA6F7",
+		red = "#F38BA8",
+		maroon = "#EBA0AC",
+		peach = "#FAB387",
+		yellow = "#F9E2AF",
+		green = "#A6E3A1",
+		teal = "#94E2D5",
+		sky = "#89DCEB",
+		sapphire = "#74C7EC",
+		blue = "#89B4FA",
+		lavender = "#B4BEFE",
+
+		text = "#CDD6F4",
+		subtext1 = "#BAC2DE",
+		subtext0 = "#A6ADC8",
+		overlay2 = "#9399B2",
+		overlay1 = "#7F849C",
+		overlay0 = "#6C7086",
+		surface2 = "#585B70",
+		surface1 = "#45475A",
+		surface0 = "#313244",
+
+		base = "#1E1E2E",
+		mantle = "#181825",
+		crust = "#11111B",
+	},
+	light = {
+		rosewater = "#dc8a78",
+		flamingo = "#DD7878",
+		pink = "#ea76cb",
+		mauve = "#8839EF",
+		red = "#D20F39",
+		maroon = "#E64553",
+		peach = "#FE640B",
+		yellow = "#df8e1d",
+		green = "#40A02B",
+		teal = "#179299",
+		sky = "#04A5E5",
+		sapphire = "#209FB5",
+		blue = "#1e66f5",
+		lavender = "#7287FD",
+
+		text = "#4C4F69",
+		subtext1 = "#5C5F77",
+		subtext0 = "#6C6F85",
+		overlay2 = "#7C7F93",
+		overlay1 = "#8C8FA1",
+		overlay0 = "#9CA0B0",
+		surface2 = "#ACB0BE",
+		surface1 = "#BCC0CC",
+		surface0 = "#CCD0DA",
+
+		base = "#FFFFFF",
+		mantle = "#E6E9EF",
+		crust = "#DCE0E8",
+	},
+}
+
+local colors = is_dark and catppuccin.dark or catppuccin.light
+
+local function get_process(tab)
+	local process_icons = {
+		["docker"] = {
+			{ Foreground = { Color = colors.blue } },
+			{ Text = wezterm.nerdfonts.linux_docker },
+		},
+		["docker-compose"] = {
+			{ Foreground = { Color = colors.blue } },
+			{ Text = wezterm.nerdfonts.linux_docker },
+		},
+		["nvim"] = {
+			{ Foreground = { Color = colors.green } },
+			{ Text = wezterm.nerdfonts.custom_vim },
+		},
+		["vim"] = {
+			{ Foreground = { Color = colors.green } },
+			{ Text = wezterm.nerdfonts.dev_vim },
+		},
+		["node"] = {
+			{ Foreground = { Color = colors.green } },
+			{ Text = wezterm.nerdfonts.mdi_hexagon },
+		},
+		["zsh"] = {
+			{ Foreground = { Color = colors.peach } },
+			{ Text = wezterm.nerdfonts.dev_terminal },
+		},
+		["bash"] = {
+			{ Foreground = { Color = colors.subtext0 } },
+			{ Text = wezterm.nerdfonts.cod_terminal_bash },
+		},
+		["paru"] = {
+			{ Foreground = { Color = colors.lavender } },
+			{ Text = wezterm.nerdfonts.linux_archlinux },
+		},
+		["htop"] = {
+			{ Foreground = { Color = colors.yellow } },
+			{ Text = wezterm.nerdfonts.mdi_chart_donut_variant },
+		},
+		["cargo"] = {
+			{ Foreground = { Color = colors.peach } },
+			{ Text = wezterm.nerdfonts.dev_rust },
+		},
+		["go"] = {
+			{ Foreground = { Color = colors.sapphire } },
+			{ Text = wezterm.nerdfonts.mdi_language_go },
+		},
+		["lazydocker"] = {
+			{ Foreground = { Color = colors.blue } },
+			{ Text = wezterm.nerdfonts.linux_docker },
+		},
+		["git"] = {
+			{ Foreground = { Color = colors.peach } },
+			{ Text = wezterm.nerdfonts.dev_git },
+		},
+		["lazygit"] = {
+			{ Foreground = { Color = colors.peach } },
+			{ Text = wezterm.nerdfonts.dev_git },
+		},
+		["lua"] = {
+			{ Foreground = { Color = colors.blue } },
+			{ Text = wezterm.nerdfonts.seti_lua },
+		},
+		["wget"] = {
+			{ Foreground = { Color = colors.yellow } },
+			{ Text = wezterm.nerdfonts.mdi_arrow_down_box },
+		},
+		["curl"] = {
+			{ Foreground = { Color = colors.yellow } },
+			{ Text = wezterm.nerdfonts.mdi_flattr },
+		},
+		["gh"] = {
+			{ Foreground = { Color = colors.mauve } },
+			{ Text = wezterm.nerdfonts.dev_github_badge },
+		},
+	}
+
+	local process_name = string.gsub(tab.active_pane.foreground_process_name, "(.*[/\\])(.*)", "%2")
+
+	return wezterm.format(
+		process_icons[process_name]
+		or { { Foreground = { Color = colors.sky } }, { Text = string.format("[%s]", process_name) } }
+	)
+end
+
+local function get_current_working_dir(tab)
+	local current_dir = tab.active_pane.current_working_dir
+	local HOME_DIR = string.format("file://%s", os.getenv("HOME"))
+
+	return current_dir == HOME_DIR and "  ~"
+			or string.format("  %s", string.gsub(current_dir, "(.*[/\\])(.*)", "%2"))
+end
+
+wezterm.on("format-tab-title", function(tab)
+	return wezterm.format({
+		{ Attribute = { Intensity = "Half" } },
+		{ Text = string.format(" %s  ", tab.tab_index + 1) },
+		"ResetAttributes",
+		{ Text = get_process(tab) },
+		{ Text = " " },
+		{ Text = get_current_working_dir(tab) },
+		{ Foreground = { Color = colors.base } },
+		{ Text = "  ▕" },
+	})
+end)
+
+wezterm.on("update-right-status", function(window)
+	window:set_right_status(wezterm.format({
+		{ Attribute = { Intensity = "Bold" } },
+		{ Text = wezterm.strftime(" %A, %d %B %Y %I:%M %p ") },
+	}))
+end)
+
 return {
-	font = wezterm.font("Hack Nerd Font Mono"),
-  bold = true,
-	font_size = 16,
-	color_scheme = "Catppuccin Frappe",
+	--	color_scheme = "Catppuccin Frappe",
+	--	check_for_updates = false,
+	font = wezterm.font_with_fallback({
+		"Hack Nerd Font Mono",
+		"Liga SFMono Nerd Font",
+		"Apple Color Emoji",
+	}),
+	font_size = 15,
+	max_fps = 120,
+	bold = true,
+	enable_wayland = false,
+	pane_focus_follows_mouse = false,
+	warn_about_missing_glyphs = false,
+	show_update_window = false,
 	check_for_updates = false,
+	line_height = 1.5,
+	window_decorations = "RESIZE",
+	window_close_confirmation = "NeverPrompt",
+	audible_bell = "Disabled",
+	window_padding = {
+		left = 0,
+		right = 0,
+		top = 0,
+		bottom = 0,
+	},
+	initial_cols = 110,
+	initial_rows = 25,
+	inactive_pane_hsb = {
+		saturation = 1.0,
+		brightness = is_dark and 0.85 or 0.95,
+	},
+	enable_scroll_bar = false,
+	tab_bar_at_bottom = true,
+	use_fancy_tab_bar = false,
+	show_new_tab_button_in_tab_bar = false,
+	window_background_opacity = 1.0,
+	tab_max_width = 50,
+	hide_tab_bar_if_only_one_tab = true,
+	disable_default_key_bindings = false,
+	front_end = "OpenGL",
+	colors = {
+		split = colors.surface0,
+		foreground = colors.text,
+		background = colors.base,
+		cursor_bg = colors.rosewater,
+		cursor_border = colors.rosewater,
+		cursor_fg = is_dark and colors.base or colors.crust,
+		selection_bg = colors.surface2,
+		selection_fg = colors.text,
+		visual_bell = colors.surface0,
+		indexed = {
+			[16] = colors.peach,
+			[17] = colors.rosewater,
+		},
+		scrollbar_thumb = colors.surface2,
+		compose_cursor = colors.flamingo,
+		ansi = {
+			is_dark and colors.subtext1 or colors.surface1,
+			colors.red,
+			colors.green,
+			colors.yellow,
+			colors.blue,
+			colors.pink,
+			colors.teal,
+			is_dark and colors.surface2 or colors.subtext1,
+		},
+		brights = {
+			is_dark and colors.subtext0 or colors.surface2,
+			colors.red,
+			colors.green,
+			colors.yellow,
+			colors.blue,
+			colors.pink,
+			colors.teal,
+			is_dark and colors.surface1 or colors.subtext0,
+		},
+		tab_bar = {
+			background = colors.crust,
+			active_tab = {
+				bg_color = "none",
+				fg_color = colors.subtext1,
+				intensity = "Bold",
+				underline = "None",
+				italic = false,
+				strikethrough = false,
+			},
+			inactive_tab = {
+				bg_color = colors.crust,
+				fg_color = colors.surface2,
+			},
+			inactive_tab_hover = {
+				bg_color = colors.mantle,
+				fg_color = colors.subtext0,
+			},
+			new_tab = {
+				bg_color = colors.crust,
+				fg_color = colors.subtext0,
+			},
+			new_tab_hover = {
+				bg_color = colors.crust,
+				fg_color = colors.subtext0,
+			},
+		},
+	},
 	keys = {
 		{
 			key = "e",
@@ -21,53 +397,6 @@ return {
 			mods = "CMD",
 			action = wezterm.action.CloseCurrentPane({ confirm = false }),
 		},
-	},
-	enable_tab_bar = true,
-	tab_bar_at_bottom = true,
-	use_fancy_tab_bar = false,
-	colors = {
-		tab_bar = {
-			background = "#232634",
-			-- The active tab is the one that has focus in the window
-			active_tab = {
-				-- The color of the background area for the tab
-				bg_color = "#ca9ee6",
-				-- The color of the text for the tab
-				fg_color = "#282a39",
-
-				-- Specify whether you want "Half", "Normal" or "Bold" intensity for the
-				-- label shown for this tab.
-				-- The default is "Normal"
-				intensity = "Bold",
-
-				-- Specify whether you want "None", "Single" or "Double" underline for
-				-- label shown for this tab.
-				-- The default is "None"
-				underline = "None",
-
-				-- Specify whether you want the text to be italic (true) or not (false)
-				-- for this tab.  The default is false.
-				italic = false,
-
-				-- Specify whether you want the text to be rendered with strikethrough (true)
-				-- or not for this tab.  The default is false.
-				strikethrough = false,
-			},
-			new_tab = {
-				bg_color = "#232634",
-				fg_color = "#c4ccec",
-				-- The same options that were listed under the `active_tab` section above
-				-- can also be used for `new_tab`.
-			},
-
-			-- Inactive tabs are the tabs that do not have focus
-			inactive_tab = {
-				bg_color = "#282c3c",
-				fg_color = "#c4ccec",
-
-				-- The same options that were listed under the `active_tab` section above
-				-- can also be used for `inactive_tab`.
-			},
-		},
+		{ key = "F11", mods = "", action = wezterm.action.ToggleFullScreen },
 	},
 }
