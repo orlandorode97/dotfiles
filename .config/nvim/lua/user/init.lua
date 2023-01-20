f9aa19ec6639791cf9e816703a16c99baeef8008
Orlando Romo <orlando.romo.97@hotmail.com>
2023-01-20 16:52:54 -0600

    add another plugin

--- .config/nvim/lua/user/init.lua
+++ .config/nvim/lua/user/init.lua
@@ -295,6 +295,35 @@ local config = {
                         { "rktjmp/lush.nvim" },
                         -- bluloco theme
                         { "uloco/bluloco.nvim" },
+                        -- modes nvim for current line
+                        { "mvllow/modes.nvim",
+                                config = function()
+                                        require('modes').setup({
+                                                colors = {
+                                                        copy = "#f5c359",
+                                                        delete = "#c75c6a",
+                                                        insert = "#78ccc5",
+                                                        visual = "#9745be",
+                                                },
+
+                                                -- Set opacity for cursorline and number background
+                                                line_opacity = 0.15,
+
+                                                -- Enable cursor highlights
+                                                set_cursor = true,
+
+                                                -- Enable cursorline initially, and disable cursorline for inactive windows
+                                                -- or ignored filetypes
+                                                set_cursorline = true,
+
+                                                -- Enable line number highlights to match cursorline
+                                                set_number = true,
+
+                                                -- Disable modes highlights in specified filetypes
+                                                -- Please PR commonly ignored filetypes
+                                                ignore_filetypes = { 'NvimTree', 'TelescopePrompt' }
+                                        })
+                                end },
                         -- oxocarbon theme
                         { "nyoom-engineering/oxocarbon.nvim" },
                         {
