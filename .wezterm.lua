3aa16502184a79579b26646f234dd522e96595c5
Orlando Romo <orlando.romo.97@hotmail.com>
2023-02-05 22:01:56 -0600

    update .wezterm.lua

--- .wezterm.lua
+++ .wezterm.lua
@@ -1,76 +1,3 @@
---local wezterm = require("wezterm")
---return {
---	font = wezterm.font("Hack Nerd Font Mono"),
---  bold = true,
---	font_size = 16,
---	color_scheme = "Catppuccin Frappe",
---	check_for_updates = false,
---	keys = {
---		{
---			key = "e",
---			mods = "CMD",
---			action = wezterm.action.SplitHorizontal({ domain = "CurrentPaneDomain" }),
---		},
---		{
---			key = "d",
---			mods = "CMD",
---			action = wezterm.action.SplitVertical({ domain = "CurrentPaneDomain" }),
---		},
---		{
---			key = "w",
---			mods = "CMD",
---			action = wezterm.action.CloseCurrentPane({ confirm = false }),
---		},
---	},
---	enable_tab_bar = true,
---	tab_bar_at_bottom = true,
---	use_fancy_tab_bar = false,
---	colors = {
---		tab_bar = {
---			background = "#232634",
---			-- The active tab is the one that has focus in the window
---			active_tab = {
---				-- The color of the background area for the tab
---				bg_color = "#ca9ee6",
---				-- The color of the text for the tab
---				fg_color = "#282a39",
---
---				-- Specify whether you want "Half", "Normal" or "Bold" intensity for the
---				-- label shown for this tab.
---				-- The default is "Normal"
---				intensity = "Bold",
---
---				-- Specify whether you want "None", "Single" or "Double" underline for
---				-- label shown for this tab.
---				-- The default is "None"
---				underline = "None",
---
---				-- Specify whether you want the text to be italic (true) or not (false)
---				-- for this tab.  The default is false.
---				italic = false,
---
---				-- Specify whether you want the text to be rendered with strikethrough (true)
---				-- or not for this tab.  The default is false.
---				strikethrough = false,
---			},
---			new_tab = {
---				bg_color = "#232634",
---				fg_color = "#c4ccec",
---				-- The same options that were listed under the `active_tab` section above
---				-- can also be used for `new_tab`.
---			},
---
---			-- Inactive tabs are the tabs that do not have focus
---			inactive_tab = {
---				bg_color = "#282c3c",
---				fg_color = "#c4ccec",
---
---				-- The same options that were listed under the `active_tab` section above
---				-- can also be used for `inactive_tab`.
---			},
---		},
---	},
---}
 local wezterm = require("wezterm")
 local is_dark = true
 
@@ -397,5 +324,51 @@ return {
 			action = wezterm.action.CloseCurrentPane({ confirm = false }),
 		},
 		{ key = "F11", mods = "", action = wezterm.action.ToggleFullScreen },
+	},
+	hyperlink_rules = {
+		-- Linkify things that look like URLs and the host has a TLD name.
+		-- Compiled-in default. Used if you don't specify any hyperlink_rules.
+		{
+			regex = '\\b\\w+://[\\w.-]+\\.[a-z]{2,15}\\S*\\b',
+			format = '$0',
+		},
+
+		-- linkify email addresses
+		-- Compiled-in default. Used if you don't specify any hyperlink_rules.
+		{
+			regex = [[\b\w+@[\w-]+(\.[\w-]+)+\b]],
+			format = 'mailto:$0',
+		},
+
+		-- file:// URI
+		-- Compiled-in default. Used if you don't specify any hyperlink_rules.
+		{
+			regex = [[\bfile://\S*\b]],
+			format = '$0',
+		},
+
+		-- Linkify things that look like URLs with numeric addresses as hosts.
+		-- E.g. http://127.0.0.1:8000 for a local development server,
+		-- or http://192.168.1.1 for the web interface of many routers.
+		{
+			regex = [[\b\w+://(?:[\d]{1,3}\.){3}[\d]{1,3}\S*\b]],
+			format = '$0',
+		},
+
+		-- Make task numbers clickable
+		-- The first matched regex group is captured in $1.
+		{
+			regex = [[\b[tT](\d+)\b]],
+			format = 'https://example.com/tasks/?t=$1',
+		},
+
+		-- Make username/project paths clickable. This implies paths like the following are for GitHub.
+		-- ( "nvim-treesitter/nvim-treesitter" | wbthomason/packer.nvim | wez/wezterm | "wez/wezterm.git" )
+		-- As long as a full URL hyperlink regex exists above this it should not match a full URL to
+		-- GitHub or GitLab / BitBucket (i.e. https://gitlab.com/user/project.git is still a whole clickable URL)
+		{
+			regex = [[["]?([\w\d]{1}[-\w\d]+)(/){1}([-\w\d\.]+)["]?]],
+			format = 'https://www.github.com/$1/$3',
+		},
 	},
 }
