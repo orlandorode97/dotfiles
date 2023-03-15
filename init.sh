1481e68feffde5bd359d8ecf076d9b910b672fb8
Orlando Romo <orlando.romo.97@hotmail.com>
2023-03-15 16:16:48 -0600

    more updates

--- init.sh
+++ init.sh
@@ -4,4 +4,12 @@ ln -s "${PWD}/.wezterm.lua" "${HOME}/.wezterm.lua"
 ln -s "${PWD}/starship.toml" "${HOME}/.config/starship.toml"
 ln -s "${PWD}/.config/nvim" "${HOME}/.config/nvim"
 echo "In progress"
+echo "Config files done!"
+
+echo "Clone wallpapers in ${HOME}"
+git clone https://github.com/rose-pine/wallpapers.git ${HOME}/wallpapers
+echo "Wallpapers cloned!"
+
+echo "Installing brew packages"
+brew install buf helix k9s kubernetes-cli make ninja cmake automake protobuf protolint ripgrep sqlc
 echo "Done!"
