32976e4666fb328934fb6f4a25491ce762d8ef63
Orlando Romo <orlando.romo.97@hotmail.com>
2023-03-20 14:39:22 -0600

    add telescope live grep with arguments

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -1,7 +1,8 @@
 return {
   "nvim-telescope/telescope.nvim",
   dependencies = {
-    { "nvim-telescope/telescope-fzf-native.nvim", enabled = vim.fn.executable "make" == 1, build = "make" },
+    { "nvim-telescope/telescope-fzf-native.nvim",    enabled = vim.fn.executable "make" == 1, build = "make" },
+    { "nvim-telescope/telescope-live-grep-args.nvim" }
   },
   cmd = "Telescope",
   opts = function()
@@ -25,7 +26,6 @@ return {
           height = 0.80,
           preview_cutoff = 120,
         },
-
         mappings = {
           i = {
             ["<C-n>"] = actions.cycle_history_next,
