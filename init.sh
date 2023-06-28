753c3f3cbb98cb3f8e7dee80627009ef658eba03
Orlando Romo <orlando.romo.97@hotmail.com>
2023-06-28 09:45:24 -0600

    update config

--- init.sh
+++ init.sh
@@ -1,18 +1,14 @@
 #!/bin/bash
 echo "Setting up config files"
-ln -s "${PWD}/.wezterm.lua" "${HOME}/.wezterm.lua"
 ln -s "${PWD}/starship.toml" "${HOME}/.config/starship.toml"
 ln -s "${PWD}/.config/nvim" "${HOME}/.config/nvim"
 ln -s "${PWD}/.config/lf" "${HOME}/.config/lf"
 ln -s "${PWD}/.config/gitui" "${HOME}/.config/gitui"
 ln -s "${PWD}/.config/gh" "${HOME}/.config/gh"
 ln -s "${PWD}/.config/wezterm" "${HOME}/.config/wezterm"
+ln -s "${PWD}/.config/gh-dash" "${HOME}/.config/gh-dash"
 echo "In progress"
 echo "Config files done!"
-
-echo "Clone wallpapers in ${HOME}"
-git clone https://github.com/rose-pine/wallpapers.git ${HOME}/wallpapers
-echo "Wallpapers cloned!"
 
 echo "Installing brew packages"
 brew install git buf helix k9s kubernetes-cli make ninja cmake automake protobuf protolint ripgrep sqlc lf gitui gh
