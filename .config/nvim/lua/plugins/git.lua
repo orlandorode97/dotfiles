48b69409e28161837ada6a8efde8a002f48eb98a
Orlando Romo <orlando.romo.97@hotmail.com>
2023-08-06 19:38:16 -0600

    remove gitsigsn

--- .config/nvim/lua/plugins/git.lua
+++ .config/nvim/lua/plugins/git.lua
@@ -2,15 +2,5 @@ return {
   "lewis6991/gitsigns.nvim",
   enabled = vim.fn.executable "git" == 1,
   event = "User AstroGitFile",
-  opts = {
-    current_line_blame = true,
-    signs = {
-      add = { text = "" },
-      change = { text = " " },
-      delete = { text = " " },
-      topdelete = { text = " " },
-      changedelete = { text = " " },
-      untracked = { text = " " },
-    },
-  },
+  opts = {}
 }
