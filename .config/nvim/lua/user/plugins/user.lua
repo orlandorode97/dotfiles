a1193b2230c0db192f8552d6c2c89e3da8400e38
Orlando Romo <orlando.romo.97@hotmail.com>
2024-01-11 23:39:36 -0600

    update starship config colors and default wezterm font

--- .config/nvim/lua/user/plugins/user.lua
+++ .config/nvim/lua/user/plugins/user.lua
@@ -11,6 +11,29 @@ return {
         name = "nvim-treesitter-context"
     },
     {
+        "folke/zen-mode.nvim",
+        event = "BufRead",
+        name = "zen-mode",
+        opts = {
+            window = {
+                options = {
+                    signcolumn = "no",      -- disable signcolumn
+                    number = false,         -- disable number column
+                    relativenumber = false, -- disable relative numbers
+                    cursorline = false,     -- disable cursorline
+                    cursorcolumn = false,   -- disable cursor column
+                    foldcolumn = "0",       -- disable fold column
+                    list = false,           -- disable whitespace characters
+                },
+            }
+        }
+    },
+    {
+        "catppuccin/nvim",
+        event = "BufRead",
+        name = "catppuccin",
+    },
+    {
         "ixru/nvim-markdown",
         event = "BufRead",
         name = "nvim-markdown"
--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -5,13 +5,38 @@ local keys = require("keys")
 
 Tab.setup()
 
+wezterm.on('user-var-changed', function(window, pane, name, value)
+	local overrides = window:get_config_overrides() or {}
+	if name == "ZEN_MODE" then
+		local incremental = value:find("+")
+		local number_value = tonumber(value)
+		if incremental ~= nil then
+			while (number_value > 0) do
+				window:perform_action(wezterm.action.IncreaseFontSize, pane)
+				number_value = number_value - 1
+			end
+			overrides.enable_tab_bar = false
+		elseif number_value < 0 then
+			window:perform_action(wezterm.action.ResetFontSize, pane)
+			overrides.font_size = nil
+			overrides.enable_tab_bar = true
+		else
+			overrides.font_size = number_value
+			overrides.enable_tab_bar = false
+		end
+	end
+	window:set_config_overrides(overrides)
+end)
+
 
 return {
 	colors = theme.colors,
 	keys = keys,
 	color_scheme = "Kolorit",
 	scrollback_lines = 50000,
-	font = wezterm.font 'Liga SFMono Nerd Font',
+	font = wezterm.font 'CaskaydiaCove Nerd Font Propo',
+	-- font = wezterm.font 'Ubuntu Nerd Font Propo',
+	-- font = wezterm.font 'InconsolataGo Nerd Font Mono',
 	font_size = 14.3,
 	max_fps = 120,
 	enable_wayland = false,
