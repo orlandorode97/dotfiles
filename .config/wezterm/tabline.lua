bfdebd7a5d36f16a1d0697f656b731cde3cbc2dc
orlando romo <orlando.romo.97@hotmail.com>
2024-08-18 22:24:18 -0600

    update wezterm config

---
+++ .config/wezterm/tabline.lua
@@ -0,0 +1,217 @@
+local wezterm = require("wezterm")
+
+local theme_colors = wezterm.color.get_builtin_schemes()["Catppuccin Mocha"]
+
+local process_icons = {
+	["docker"] = wezterm.nerdfonts.linux_docker,
+	["docker-compose"] = wezterm.nerdfonts.linux_docker,
+	["psql"] = wezterm.nerdfonts.dev_postgresql,
+	["kubectl"] = wezterm.nerdfonts.fa_cubes,
+	["stern"] = wezterm.nerdfonts.linux_docker,
+	["nvim"] = wezterm.nerdfonts.custom_vim,
+	["make"] = wezterm.nerdfonts.dev_gnu,
+	["vim"] = wezterm.nerdfonts.dev_vim,
+	["node"] = wezterm.nerdfonts.mdi_hexagon,
+	["go"] = wezterm.nerdfonts.seti_go,
+	["zsh"] = wezterm.nerdfonts.dev_terminal,
+	["bash"] = wezterm.nerdfonts.cod_terminal_bash,
+	["btm"] = wezterm.nerdfonts.mdi_chart_donut_variant,
+	["htop"] = wezterm.nerdfonts.mdi_chart_donut_variant,
+	["cargo"] = wezterm.nerdfonts.dev_rust,
+	["sudo"] = wezterm.nerdfonts.fa_hashtag,
+	["lazydocker"] = wezterm.nerdfonts.linux_docker,
+	["git"] = wezterm.nerdfonts.dev_git,
+	["lua"] = wezterm.nerdfonts.seti_lua,
+	["wget"] = wezterm.nerdfonts.mdi_arrow_down_box,
+	["curl"] = "",
+	["gh"] = wezterm.nerdfonts.dev_github_badge,
+	["ruby"] = wezterm.nerdfonts.cod_ruby,
+	["gitui"] = wezterm.nerdfonts.dev_git,
+	["lazygit"] = wezterm.nerdfonts.dev_git,
+	["lf"] = wezterm.nerdfonts.mdi_file_tree,
+	["cloud_sql_proxy"] = wezterm.nerdfonts.dev_google_cloud_platform,
+	["ssh"] = wezterm.nerdfonts.dev_google_cloud_platform,
+}
+
+-- wezterm.on("new-tab-button-click", function(window, pane, button, default_action)
+--   -- just log the default action and allow wezterm to perform it
+--   wezterm.log_info("new-tab", window, pane, button, default_action)
+-- end)
+--
+
+-- The filled in variant of the > symbol
+local SOLID_RIGHT_ARROW = wezterm.nerdfonts.pl_left_hard_divider
+
+local function get_process(tab)
+	if not tab.active_pane or tab.active_pane.foreground_process_name == "" then
+		return tab.active_pane.foreground_process_name
+	end
+
+	local process_name = string.gsub(tab.active_pane.foreground_process_name, "(.*[/\\])(.*)", "%2")
+	if string.find(process_name, "kubectl") then
+		process_name = "kubectl"
+	elseif string.find(process_name, "nvim") then
+		process_name = "nvim"
+	end
+
+	return process_icons[process_name] or string.format("[%s]", process_name)
+end
+
+-- wezterm.on("update-right-status", function(window, pane)
+--   -- demonstrates shelling out to get some external status.
+--   -- wezterm will parse escape sequences output by the
+--   -- child process and include them in the status area, too.
+--   local success, date, stderr = wezterm.run_child_process { "date" }
+--
+--   -- However, if all you need is to format the date/time, then:
+--   date = wezterm.strftime "%Y-%m-%d %H:%M:%S"
+--
+--   -- Make it italic and underlined
+--   window:set_right_status(wezterm.format {
+--     { Attribute = { Underline = "Single" } },
+--     { Attribute = { Italic = true } },
+--     { Text = "Hello " .. date },
+--   })
+-- end)
+
+wezterm.on("format-tab-title", function(tab, tabs, panes, config, hover, max_width)
+	local has_unseen_output = false
+	if not tab.is_active then
+		for _, pane in ipairs(tab.panes) do
+			if pane.has_unseen_output then
+				has_unseen_output = true
+				break
+			end
+		end
+	end
+
+	local edge_background = theme_colors.tab_bar["inactive_tab_edge"]
+	local background = theme_colors.tab_bar.inactive_tab["bg_color"]
+	local foreground = theme_colors.tab_bar.inactive_tab["fg_color"]
+
+	if tab.is_active then
+		background = theme_colors.tab_bar.active_tab["bg_color"]
+		foreground = theme_colors.tab_bar.active_tab["fg_color"]
+	elseif hover then
+		background = theme_colors.tab_bar.inactive_tab_hover["bg_color"]
+		foreground = theme_colors.tab_bar.inactive_tab_hover["fg_color"]
+	end
+
+	local edge_foreground = background
+
+	-- local title = tab_title(tab)
+
+	-- ensure that the titles fit in the available space,
+	-- and that we have room for the edges.
+	-- title = wezterm.truncate_right(title, max_width - 2)
+	--
+
+	local title = string.format(" %s %s ", tab.tab_index + 1, get_process(tab))
+
+	if has_unseen_output then
+		return {
+			{ Background = { Color = edge_foreground } },
+			{ Foreground = { Color = edge_background } },
+			{ Text = SOLID_RIGHT_ARROW },
+			{ Background = { Color = background } },
+			{ Foreground = { Color = theme_colors.brights[7] } },
+			{ Text = title },
+			{ Background = { Color = edge_background } },
+			{ Foreground = { Color = edge_foreground } },
+			{ Text = SOLID_RIGHT_ARROW },
+		}
+	end
+
+	return {
+		{ Background = { Color = edge_foreground } },
+		{ Foreground = { Color = edge_background } },
+		{ Text = SOLID_RIGHT_ARROW },
+		{ Background = { Color = background } },
+		{ Foreground = { Color = foreground } },
+		{ Text = title },
+		{ Background = { Color = edge_background } },
+		{ Foreground = { Color = edge_foreground } },
+		{ Text = SOLID_RIGHT_ARROW },
+	}
+end)
+
+wezterm.on("update-right-status", function(window, pane)
+	-- Each element holds the text for a cell in a "powerline" style << fade
+	local cells = {}
+
+	-- Figure out the cwd and host of the current pane.
+	-- This will pick up the hostname for the remote host if your
+	-- shell is using OSC 7 on the remote host.
+	local cwd_uri = pane:get_current_working_dir()
+	if cwd_uri then
+		cwd_uri = cwd_uri.path:sub(8)
+		local slash = cwd_uri:find("/")
+		local cwd = ""
+		local hostname = ""
+		if slash then
+			hostname = cwd_uri:sub(1, slash - 1)
+			-- Remove the domain name portion of the hostname
+			local dot = hostname:find("[.]")
+			if dot then
+				hostname = hostname:sub(1, dot - 1)
+			end
+			-- and extract the cwd from the uri
+			cwd = cwd_uri:sub(slash)
+
+			table.insert(cells, cwd)
+			table.insert(cells, hostname)
+		end
+	end
+
+	-- I like my date/time in this style: "Wed Mar 3 08:14"
+	local date = wezterm.strftime("%a %b %-d %H:%M")
+	table.insert(cells, date)
+
+	-- An entry for each battery (typically 0 or 1 battery)
+	for _, b in ipairs(wezterm.battery_info()) do
+		table.insert(cells, string.format("%.0f%%", b.state_of_charge * 100))
+	end
+
+	-- The powerline < symbol
+	-- The filled in variant of the < symbol
+	local SOLID_LEFT_ARROW = utf8.char(0xe0b2)
+
+	-- Color palette for the backgrounds of each cell
+	local colors = {
+		theme_colors.brights[0],
+		"#3c1361",
+		"#52307c",
+		"#663a82",
+		"#7c5295",
+		"#b491c8",
+	}
+	-- local colors = theme_colors.ansi
+
+	-- Foreground color for the text across the fade
+	local text_fg = theme_colors.cursor_fg
+
+	-- The elements to be formatted
+	local elements = {}
+	-- How many cells have been formatted
+	local num_cells = 0
+
+	-- Translate a cell into elements
+	local function push(text, is_last)
+		local cell_no = num_cells + 1
+		table.insert(elements, { Foreground = { Color = text_fg } })
+		table.insert(elements, { Background = { Color = colors[cell_no] } })
+		table.insert(elements, { Text = " " .. text .. " " })
+		if not is_last then
+			table.insert(elements, { Foreground = { Color = colors[cell_no + 1] } })
+			table.insert(elements, { Text = SOLID_LEFT_ARROW })
+		end
+		num_cells = num_cells + 1
+	end
+
+	while #cells > 0 do
+		local cell = table.remove(cells, 1)
+		push(cell, #cells == 0)
+	end
+
+	window:set_right_status(wezterm.format(elements))
+end)
--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,92 +1,70 @@
 local wezterm = require("wezterm")
-local Tab = require("tab")
-local theme = require("theme")
 local keys = require("keys")
 
-Tab.setup()
+local config = {}
 
-wezterm.on("user-var-changed", function(window, pane, name, value)
-  local overrides = window:get_config_overrides() or {}
-  if name == "ZEN_MODE" then
-    local incremental = value:find("+")
-    local number_value = tonumber(value)
-    if incremental ~= nil then
-      while number_value > 0 do
-        window:perform_action(wezterm.action.IncreaseFontSize, pane)
-        number_value = number_value - 1
-      end
-      overrides.enable_tab_bar = false
-    elseif number_value < 0 then
-      window:perform_action(wezterm.action.ResetFontSize, pane)
-      overrides.font_size = nil
-      overrides.enable_tab_bar = true
-    else
-      overrides.font_size = number_value
-      overrides.enable_tab_bar = false
-    end
-  end
-  window:set_config_overrides(overrides)
-end)
+if wezterm.config_builder then
+	config = wezterm.config_builder()
+end
 
-return {
-  keys = keys,
-  color_scheme = "Snazzy",
-  scrollback_lines = 50000,
-  font = wezterm.font("Lilex Nerd Font", { weight = "DemiBold" }),
-  font_size = 14.5,
-  max_fps = 120,
-  enable_wayland = false,
-  warn_about_missing_glyphs = false,
-  show_update_window = false,
-  check_for_updates = false,
-  line_height = 1.1,
-  window_close_confirmation = "NeverPrompt",
-  audible_bell = "Disabled",
-  window_padding = {
-    left = 0,
-    right = 0,
-    top = 0,
-    bottom = 0,
-  },
-  initial_cols = 110,
-  initial_rows = 25,
-  enable_scroll_bar = false,
-  tab_bar_at_bottom = true,
-  use_fancy_tab_bar = false,
-  show_new_tab_button_in_tab_bar = false,
-  macos_window_background_blur = 30,
-  enable_tab_bar = true,
-  tab_max_width = 50,
-  window_decorations = "RESIZE",
-  hide_tab_bar_if_only_one_tab = true,
-  disable_default_key_bindings = false,
-  front_end = "OpenGL",
-  default_cursor_style = "BlinkingUnderline",
-  hyperlink_rules = {
-    {
-      regex = "\\b\\w+://[\\w.-]+:[0-9]{2,15}\\S*\\b",
-      format = "$0",
-    },
-    {
-      regex = "\\b\\w+://[\\w.-]+\\.[a-z]{2,15}\\S*\\b",
-      format = "$0",
-    },
-    {
-      regex = [[\b\w+@[\w-]+(\.[\w-]+)+\b]],
-      format = "mailto:$0",
-    },
-    {
-      regex = [[\bfile://\S*\b]],
-      format = "$0",
-    },
-    {
-      regex = [[\b\w+://(?:[\d]{1,3}\.){3}[\d]{1,3}\S*\b]],
-      format = "$0",
-    },
-    {
-      regex = [[\b[tT](\d+)\b]],
-      format = "https://example.com/tasks/?t=$1",
-    },
-  },
-  window_background_opacity = 0.9,
+config.window_decorations = "RESIZE"
+config.color_scheme = "Catppuccin Mocha"
+config.font = wezterm.font("JetBrainsMono Nerd Font", { weight = "Regular" })
+config.font_size = 14.5
+config.use_fancy_tab_bar = false
+config.tab_bar_at_bottom = true
+config.window_background_opacity = 0.9
+config.macos_window_background_blur = 50
+
+config.max_fps = 120
+config.enable_wayland = false
+config.warn_about_missing_glyphs = false
+config.show_update_window = false
+config.check_for_updates = false
+config.default_cursor_style = "SteadyUnderline"
+config.line_height = 1.1
+config.window_close_confirmation = "NeverPrompt"
+config.audible_bell = "Disabled"
+
+local function windowicons()
+	local CLOSE_CROSS = ("  %s  "):format(wezterm.nerdfonts.md_close)
+	local MAXIMISE_BOX = (" %s "):format(wezterm.nerdfonts.md_window_maximize)
+	local HIDE_DASH = (" %s "):format(wezterm.nerdfonts.md_window_minimize)
+	local PLUS = (" %s "):format(wezterm.nerdfonts.md_plus)
+
+	---@param icon string
+	---@param hover_idx number | nil
+	local function window_icons(icon, hover_idx)
+		if hover_idx ~= nil then
+			return wezterm.format({
+				{ Background = { Color = wezterm.color.get_default_colors().brights[hover_idx] } },
+				{ Text = icon },
+			})
+		else
+			return wezterm.format({ { Text = icon } })
+		end
+	end
+
+	return {
+		new_tab = window_icons(PLUS),
+		new_tab_hover = window_icons(PLUS),
+		window_close = window_icons(CLOSE_CROSS),
+		window_maximize_hover = window_icons(MAXIMISE_BOX, 5),
+		window_hide = window_icons(HIDE_DASH),
+		window_hide_hover = window_icons(HIDE_DASH),
+	}
+end
+
+config.tab_bar_style = windowicons()
+config.leader = { key = "a", mods = "CTRL", timeout_milliseconds = 1000 }
+config.keys = keys
+
+config.launch_menu = {
+	{ args = { "top" } },
+	{ args = { "nvim", "." } },
 }
+-- require("statusbar.right")
+require("tabline")
+
+-- and finally, return the configuration to wezterm
+return config
